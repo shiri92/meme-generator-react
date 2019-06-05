e5106ff1c8cea3796ebfabf8a202b65fb726166c
shiri92 <shiriron92@gmail.com>
2019-06-06 00:18:01 +0300

    Updates

---
+++ precache-manifest.0bdd58e0f6b1e4d4e87887c706eeb3ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d201c3e80f44e254a3e32c55e97d2801",
+    "url": "/meme-generator-react/index.html"
+  },
+  {
+    "revision": "8188a0cd155b02a98aaa",
+    "url": "/meme-generator-react/static/css/2.ee4fd42b.chunk.css"
+  },
+  {
+    "revision": "aba68d498509d60ed0c7",
+    "url": "/meme-generator-react/static/css/main.c243666d.chunk.css"
+  },
+  {
+    "revision": "8188a0cd155b02a98aaa",
+    "url": "/meme-generator-react/static/js/2.2a06f5a7.chunk.js"
+  },
+  {
+    "revision": "aba68d498509d60ed0c7",
+    "url": "/meme-generator-react/static/js/main.a21b66b4.chunk.js"
+  },
+  {
+    "revision": "d974404a6047f1468756",
+    "url": "/meme-generator-react/static/js/runtime~main.1097fe1c.js"
+  },
+  {
+    "revision": "be324d5b695be0894732e4e02e8fcc9f",
+    "url": "/meme-generator-react/static/media/TrollFace.be324d5b.png"
+  }
+]);
